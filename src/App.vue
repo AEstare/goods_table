3666ac292bad13b070b6128e749c9a4df34de803
Ae <2662534305@qq.com>
2023-06-20 23:45:25 +0800

    页面实现

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,56 @@
 <template>
   <div>
-    <h3>App 根组件</h3>
+    <h3>商品管理</h3>
+    <hr />
+
+    <!-- 引入表格组件 -->
+    <Table :data="goodslist">
+      <!-- 表格标题 -->
+      <template v-slot:header>
+        <th>#</th>
+        <th>商品名称</th>
+        <th>价格</th>
+        <th>标签</th>
+        <th>操作</th>
+      </template>
+      <!-- 商品表格 -->
+      <template v-slot:body="{ row, index }">
+        <td>{{ index + 1 }}</td>
+        <td>{{ row.goods_name }}</td>
+        <td>{{ row.goods_price }}</td>
+        <td>{{ row.tags.join('，') }}</td>
+        <td>
+          <button type="button" class="btn-danger btn-sm">删除</button>
+        </td>
+      </template>
+    </Table>
   </div>
 </template>
 
 <script>
+import Table from './components/Table.vue'
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      // 商品列表数据
+      goodslist: []
+    }
+  },
+  components: {
+    Table
+  },
+  methods: {
+    async getGoodsList() {
+      const { data: res } = await this.$http.get('/api/get')
+      if (res.status !== 0) return console.log('商品获取失败')
+      this.goodslist = res.data
+    }
+  },
+  created() {
+    // 发起请求
+    this.getGoodsList()
+  }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
 import { createApp } from 'vue'
-import './style.css'
 import App from './App.vue'
+import './assets/css/bootstrap.css'
+import './style.css'
+import axios from 'axios'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+axios.defaults.baseURL = 'http://127.0.0.1:4396'
+app.config.globalProperties.$http = axios
+  
+app.mount('#app')
